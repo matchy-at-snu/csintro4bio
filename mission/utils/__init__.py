a4429afcf0a95632fadffe16dcaa83fc4d6ca7f5
matchy233 <matchy233@gmail.com>
2024-10-18 05:00:14 -0400

    Add documentation to uitl functions

--- mission/utils/__init__.py
+++ mission/utils/__init__.py
@@ -4,13 +4,29 @@
 import pathlib
 
 def get_project_root() -> pathlib.Path:
+    """
+    Get the path to the root of the project
+
+    :return: The path to the root of the project
+    """
     return pathlib.Path(__file__).parent.parent.parent
 
 def get_data_dir() -> pathlib.Path:
+    """
+    Get the path to the data directory
+
+    :return: The path to the data directory
+    """
     project_root = get_project_root()
     return project_root / "data"
 
 def get_species_genome_dir(species: str) -> pathlib.Path:
+    """
+    Get the path to the genome directory for a given species
+
+    :param species: The species to get the genome directory for; one of "human", "chicken", "fruitfly", or "worm"
+    :return: The path to the genome directory for the given species
+    """
     data_dir = get_data_dir()
     if species == "human":
         return data_dir / "hg38" / "chroms"
